34a9523a87274bb961a8461144974a55111782e7
Eleazar Hernández <eleazar87@gmail.com>
2019-10-11 19:19:11 +0100

    Initial commit

---
+++ src/main/java/com/eleazar/polling/pollerAvailableVehicles/PollerAvailableVehiclesApplication.java
@@ -0,0 +1,13 @@
+package com.eleazar.polling.pollerAvailableVehicles;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PollerAvailableVehiclesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PollerAvailableVehiclesApplication.class, args);
+	}
+
+}
